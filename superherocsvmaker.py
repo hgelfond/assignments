d2feb61631f6b654a4c210c96712bc279b1ad751
Hilary Gelfond <hilary.gelfond@gmail.com>
2019-01-16 10:21:43 -0500

    superheroes hw

--- superherocsvmaker.py
+++ superherocsvmaker.py
@@ -18,12 +18,18 @@ with open('members.csv', 'w') as f:
     writer = csv.writer(f)
     header = ['name', 'age', 'secretIdentity', 'powers', 'squadName', 'homeTown', 'formed', 'secretBase', 'active']
     writer.writerow(header)
-    for member in members
-    row =  [
-    	member['name'],
-    	member['name']
-    	member['age', 'secretIdentity', 'powers', 'squadName', 'homeTown', 'formed', 'secretBase', 'active'
-    
+    for member in members:
+        row =  [
+        	member['name'],
+        	member['age'],
+            member['secretIdentity'],
+            str(member['powers']),
+            # member['squadName'],
+            # member['homeTown'],
+            # member['formed'],
+            # member['secretBase'],
+            # member['active']
+        
 
-    ]
-    writer.writerow(row)
+        ]
+        writer.writerow(row)
